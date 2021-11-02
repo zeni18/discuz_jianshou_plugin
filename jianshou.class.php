0c2ba4befbde1226a8225c0a777b70018ed912cf
cailei <cailei@ling.ai>
2021-11-03 02:54:45 +0800

    init

--- jianshou.class.php
+++ jianshou.class.php
@@ -23,17 +23,17 @@ class plugin_jianshou_forum extends plugin_jianshou
 
     public function post_bottom()
     {
-        return '<script src="//sns.io/js/min/discuz.js" charset="utf-8" type="text/javascript"></script>';
+        return '<script src="//yunshou.snscz.com/js/min/discuz.js" charset="utf-8" type="text/javascript"></script>';
     }
 
     public function viewthread_bottom()
     {
-        return '<script src="//sns.io/js/embed.js" charset="utf-8" type="text/javascript"></script>';
+        return '<script src="//yunshou.snscz.com/js/embed.js" charset="utf-8" type="text/javascript"></script>';
     }
 
     private function getCss()
     {
-        $css = '<link rel="stylesheet" type="text/css" href="//sns.io/lib/layer/skin/layer.css" />';
+        $css = '<link rel="stylesheet" type="text/css" href="//yunshou.snscz.com/lib/layer/skin/layer.css" />';
         $css .= '<style type="text/css">';
         $css .= '#e_snspay{background:url(/source/plugin/snsiopay/img/snspay-icon.png) 0 -1px no-repeat;}';
         $css .= '.b1r #e_snspay{background:url(/source/plugin/snsiopay/img/snspay-icon.png) 0 -1px no-repeat;}';
