55854d996f73c3f78870c1f776edefa3c1f6f866
cailei <cailei@ling.ai>
2021-11-03 03:07:47 +0800

    init

--- jianshou.class.php
+++ jianshou.class.php
@@ -16,28 +16,29 @@ class plugin_jianshou_forum extends plugin_jianshou
     public function post_editorctrl_left()
     {
         $html = $this->getCss();
-        $html .= '<a id="e_snspay" href="javascript:void(0);">出售</a>';
+        $html .= '<a id="add_radishpay" href="javascript:void(0);">出售</a>';
 
         return $html;
     }
 
     public function post_bottom()
     {
-        return '<script src="//yunshou.snscz.com/js/min/discuz.js" charset="utf-8" type="text/javascript"></script>';
+        return '<script src="https://jianshou.online/js/wordpress.js" charset="utf-8" type="text/javascript"></script>';
     }
 
     public function viewthread_bottom()
     {
-        return '<script src="//yunshou.snscz.com/js/embed.js" charset="utf-8" type="text/javascript"></script>';
+
+        return '<script src="https://jianshou.online/js/embed.js" charset="utf-8" type="text/javascript"></script>';
     }
 
     private function getCss()
     {
-        $css = '<link rel="stylesheet" type="text/css" href="//yunshou.snscz.com/lib/layer/skin/layer.css" />';
+        $css = '<link rel="stylesheet" type="text/css" href="https://jianshou.online/layer/layer.css" />';
         $css .= '<style type="text/css">';
-        $css .= '#e_snspay{background:url(/source/plugin/jianshou_online/img/snspay-icon.png) 0 -1px no-repeat;}';
-        $css .= '.b1r #e_snspay{background:url(/source/plugin/jianshou_online/img/snspay-icon.png) 0 -1px no-repeat;}';
-        $css .= '.b2r #e_snspay{background-position: -7px -47px;}';
+        $css .= '#add_radishpay{background:url(/source/plugin/jianshou_online/img/radishpay-icon.png) 0 -1px no-repeat;}';
+        $css .= '.b1r #add_radishpay{background:url(/source/plugin/jianshou_online/img/radishpay-icon.png) 0 -1px no-repeat;}';
+        $css .= '.b2r #add_radishpay{background-position: -7px -47px;}';
         $css .= '</style>';
 
         return $css;
