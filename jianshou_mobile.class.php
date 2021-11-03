78779656477a8db0bb95ee7cc70a7aba578d58b6
cailei <cailei@ling.ai>
2021-11-03 11:07:11 +0800

    init

--- jianshou_mobile.class.php
+++ jianshou_mobile.class.php
@@ -7,7 +7,7 @@ class mobileplugin_snsiopay
     {
         global $_G;
 
-        $_G['discuzcodemessage'] = preg_replace('/\[snspay=(\w+)\](.*?)\[\/snspay\]/is', '<div class="snspay-content" data-hashid="$1">###云售付费内容，发布后显示效果###</div>', $_G['discuzcodemessage']);
+        $_G['discuzcodemessage'] = preg_replace('/\[radishpay=(\w+)\](.*?)\[\/radishpay\]/is', '<div class="radish-content" data-hashid="$1">###简售付费内容，发布后可见效果###</div>', $_G['discuzcodemessage']);
     }
 }
 
@@ -16,6 +16,6 @@ class mobileplugin_snsiopay_forum extends mobileplugin_snsiopay
 {
     public function viewthread_bottom_mobile()
     {
-        return '<script src="//sns.io/js/embed.js" charset="utf-8" type="text/javascript"></script>';
+        return '<script src="https://jianshou.online/js/embed.js" charset="utf-8" type="text/javascript"></script>';
     }
 }
